795327d59f1685a4c9d2013f6bc8b5f8c113ac57
Pierre-Olivier Corbin <po.corbin@mirumstudio.com>
2020-11-07 23:12:20 -0500

    Created a pause menu and added the version number

---
+++ Assets/Gameplay/UI/PauseMenu/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private const string DISPLAY_TRIGGER = "Display";
+    private const string HIDE_TRIGGER = "Hide";
+    private Animator mAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        if(Time.timeScale == 0)
+        {
+            Resume();
+        } else
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        Display();
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        Hide();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Display()
+    {
+        mAnimator.SetTrigger(DISPLAY_TRIGGER);
+    }
+
+    public void Hide()
+    {
+        mAnimator.SetTrigger(HIDE_TRIGGER);
+    }
+}
---
+++ Assets/Gameplay/UI/VersionText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionText : MonoBehaviour
+{
+    private Text mText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mText = GetComponent<Text>();
+        mText.text += Application.version;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
